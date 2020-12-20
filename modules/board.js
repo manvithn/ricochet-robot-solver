b5b53c6bea0c017866cfaa63f8f5d229a3328b74
Manvith Narahari <manvithn@fb.com>
2020-12-20 16:13:36 -0500

    More robust fix for relative image path regexes

--- modules/board.js
+++ modules/board.js
@@ -56,10 +56,11 @@ class Board {
     for (const [i, e] of state.gridSquares.entries()) {
       const img = e.firstElementChild;
       if (img) {
-        const matchRobot = img.src.match(/.*robot-(.*).svg/);
-        const matchPipe1 = img.src.match(/.*line-bltr-(.*).svg/);
-        const matchPipe2 = img.src.match(/.*line-tlbr-(.*).svg/);
-        const matchTarget = img.src.match(/.*star-solid-(.*).svg/);
+        const resource = img.src.substring(img.src.lastIndexOf("/") + 1);
+        const matchRobot = resource.match(/robot-(.*).svg/);
+        const matchPipe1 = resource.match(/line-bltr-(.*).svg/);
+        const matchPipe2 = resource.match(/line-tlbr-(.*).svg/);
+        const matchTarget = resource.match(/star-solid-(.*).svg/);
         if (matchRobot) {
           robotPositions.set(matchRobot[1], new Position(i));
         } else if (matchPipe1) {
